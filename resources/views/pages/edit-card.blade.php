dd2e45d2d1c8ab0e3e41c55812736678b56d06b1
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-12-07 20:01:39 -0500

    Edit Card now looks like Create Card

--- resources/views/pages/edit-card.blade.php
+++ resources/views/pages/edit-card.blade.php
@@ -7,19 +7,35 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Update Card
+                        Edit Card
                     </div>
                     <div class="panel-body">
                         <form method="post" action="/cards/{{ $card->id }}">
                             {{ csrf_field() }}
                             {{ method_field('PUT') }}
-                            <label>Front:</label>
-                            <input type="text" name="front" placeholder="Something on the front" value="{{ $card->front }}">
-                            <br>
-                            <label>Back:</label>
-                            <input type="text" name="back" placeholder="Something on the back" value="{{ $card->back }}">
-                            <br>
-                            <input type="submit" name="save_more" value="Save">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Front
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="col-md-12">
+                                            <input class="form-control" type="text" name="front" value="{{ $card->front }}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        Back
+                                    </div>
+                                    <div class="panel-body">
+                                        <input class="form-control" type="text" name="back" value="{{ $card->back }}">
+                                    </div>
+                                </div>
+                            </div>
+                            <input class="btn btn-success" type="submit" name="save" value="Save" style="float: right">
                             <input type="hidden" name="deck_id" value="{{ $card->deck_id }}">
 
                             {{--Display errors--}}
@@ -31,6 +47,7 @@
                                 </ul>
                             @endif
                         </form>
+                        <a class="btn btn-danger" href="/decks/{{ $card->deck_id }}">Cancel</a>
                     </div>
                 </div>
             </div>
