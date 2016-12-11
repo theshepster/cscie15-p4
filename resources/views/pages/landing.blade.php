0ed2c44c92566cff77e85a3d552652c7cb6d10bd
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-12-10 23:10:51 -0500

    Lower res images to make landing page load faster.

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -16,7 +16,7 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img class="first-slide" src="https://static.pexels.com/photos/53459/lightning-storm-weather-sky-53459.jpeg" alt="Card">
+                <img class="first-slide" src={{ URL::asset('img/lightning.jpg') }} alt="Card">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>FlashyCards.</h1>
@@ -29,7 +29,7 @@
                 </div>
             </div>
             <div class="item">
-                <img class="second-slide" src="https://static.pexels.com/photos/21827/night.jpg" alt="Review">
+                <img class="second-slide" src={{ URL::asset('img/playingcards.jpg') }} alt="Review">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>Review</h1>
@@ -42,7 +42,7 @@
                 </div>
             </div>
             <div class="item">
-                <img class="third-slide" src="https://static.pexels.com/photos/211157/pexels-photo-211157.jpeg" alt="More">
+                <img class="third-slide" src={{ URL::asset('img/coal.jpg') }} alt="More">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>More Projects.</h1>
@@ -63,21 +63,21 @@
         <!-- Three columns of text below the carousel -->
         <div class="row">
             <div class="col-lg-4">
-                <img class="img-circle" src="http://cdn.biggestonline.co.uk/thurtelldesigns/images/flashy-designs.png" alt="Card" width="140" height="140">
+                <img class="img-circle" src={{ URL::asset('img/flashlogo.png') }} alt="Card" width="140" height="140">
                 <h2>So Flashy</h2>
                 <p>Want to learn something? Be flashy about it.</p>
                 <p><a class="btn btn-default" href="/home" role="button">Be Flashy »</a><br></p>
             </div>
             <!-- /.col-lg-4 -->
             <div class="col-lg-4">
-                <img class="img-circle" src="http://www.clipartkid.com/images/780/deck-of-cards-sports-and-recreation-great-clipart-for-fYbhsd-clipart.jpg" alt="Deck" width="140" height="140">
+                <img class="img-circle" src={{ URL::asset('img/deckofcards.jpg') }} alt="Deck" width="140" height="140">
                 <h2>Decked Out</h2>
                 <p>Deck yourself before you wreck yourself.</p>
                 <p><a class="btn btn-default" href="/home" role="button">Deck It Out »</a></p>
             </div>
             <!-- /.col-lg-4 -->
             <div class="col-lg-4">
-                <img class="img-circle" src="http://www.clipartkid.com/images/158/picture-of-plus-sign-clipart-best-D40BLA-clipart.png" alt="More" width="140" height="140">
+                <img class="img-circle" src={{ URL::asset('img/plussign.png') }} alt="More" width="140" height="140">
                 <h2>More Projects</h2>
                 <p>This is project 4 of 4 in Dynamic Web Applications, CSCIE-15, Fall 2016. Check out the rest of this semester's projects.</p>
                 <p><a class="btn btn-default" href="http://p1.mooreberg.me" role="button">More »</a><br></p>
